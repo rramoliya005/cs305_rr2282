b34699e3e0d6013c940091927ca0a4bba43ecc42
rramoliya005 <82795154+rramoliya005@users.noreply.github.com>
2021-12-09 22:08:33 -0700

    script file

---
+++ test.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+
+Time=$ (date +"%T")
+
+File="lines.txt"
+
+Message=$Time
+
+Lines=$(cat $File)
+
+for Line in $Lines
+do 
+    echo "$Time $Line" > test.log
+done
+
+
+git add .
+git commit -m "files updated at $Time"
+git push
